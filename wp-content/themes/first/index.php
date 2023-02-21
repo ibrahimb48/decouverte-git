8465c9b1fd44c3c2163334a2ea471c9eeff0e0c4
ibrahim <ibrahim.beghdadi48@gmail.com>
2023-02-21 10:18:11 +0100

    presentation hierarchique

--- wp-content/themes/first/index.php
+++ wp-content/themes/first/index.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
-            <!-- get_template_directory_uri(
-                http://localhost
-            ) -->
-    <?php wp_head() ?>
-</head>
-<body <?php body_class() ?>>
-    <header class="bg-primary">
-        <nav class="navbar navbar-expand container navbar-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href="" class="nav-link">Accueil</a></li>
-            </ul>
-        </nav>
-    </header>
+<?php get_header() ?>
     <main class="container">
         <div class="row">
             <!-- loop wordpress -->
@@ -48,6 +27,7 @@
             <?php endwhile ?>
         </div>
     </main>
-    <?php wp_footer() ?>
-</body>
-</html>
+<?php get_footer() ?>
+<!-- require include de php -->
+
+  
